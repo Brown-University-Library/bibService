609b7faa526f48f72c7b145ff1b0e6d9c0ade842
Hector Correa <hector_correa@brown.edu>
2018-04-02 16:20:23 -0400

    Make settings file required (with no default value)

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-	var settingsFile string
-	flag.StringVar(&settingsFile, "settings", "settings.json", "Required. Settings file")
-	flag.Parse()
+	if len(os.Args) < 2 {
+		displayHelp("No settings file indicated")
+		return
+	}
+	settingsFile := os.Args[1]
 	StartWebServer(settingsFile)
+}
+
+func displayHelp(msg string) {
+	syntax := `
+
+Syntax:
+	bibService settingsFile
+
+where settingsFile is the name of the JSON file with the configuration to use.
+Content of settingsFile should be as follows:
+
+	{
+	  "serverAddress": "localhost:9001",
+	  "sierraUrl": "https://your-iii-domain/iii/sierra-api/v5",
+	  "keySecret": "your-key:your-secret",
+	  "sessionFile": "iii_session.json"
+	}
+	`
+	fmt.Printf("%s%s\r\n", msg, syntax)
 }
