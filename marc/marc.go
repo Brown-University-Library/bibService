be6763ad16d1bd6d7e2e8e06a0d75531383c9878
Hector Correa <hector_correa@brown.edu>
2018-05-11 17:43:46 -0400

    Documentation

---
+++ marc/marc.go
@@ -0,0 +1,5 @@
+// Package MARC includes structs and functions to operate on MARC records.
+//
+// Struct MarcField represents a single field, whereas MarcFields is meant
+// to represent a collection of fields (e.g. a MARC record)
+package marc
