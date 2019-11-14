bbf8e0b6cbb29450abc18c89b35708f7ca5f504b
Hector Correa <hector_correa@brown.edu>
2019-11-14 13:42:31 -0500

    Integrated new query for pullslips

--- sierra/pullSlips.go
+++ sierra/pullSlips.go
@@ -11,7 +11,10 @@ import (
 )
 
 type PullSlipRow struct {
+	FlagDate       sql.NullTime
 	DisplayOrder   int
+	ProjectTitle   string
+	ListDate       sql.NullTime
 	OrderNum       string
 	CallNumber     string
 	CopyNum        int
@@ -25,10 +28,12 @@ type PullSlipRow struct {
 	Title          string
 	Edition        string
 	Publisher      string
+	PubYear        string
+	Author         string
 	Description    string
 	ItemLocation   string
 	LocalNotes     string
-	BndWidth       bool
+	BndWith        bool
 }
 
 func (row PullSlipRow) String() string {
@@ -49,92 +54,117 @@ func PullSlipsForList(connString string, listID int) ([]PullSlipRow, error) {
 	sqlSelect := `
 	DROP TABLE IF EXISTS temp_item_data;
 	DROP TABLE IF EXISTS temp_dupe;
-	CREATE TEMP TABLE temp_item_data AS
 
+	CREATE TEMP TABLE temp_item_data AS
 	SELECT
-	bo.display_order,
-	(
-		SELECT o.record_type_code || o.record_num || 'a'
-		FROM sierra_view.order_view as o, sierra_view.bib_record_order_record_link as ol
-		WHERE o.id = ol.order_record_id AND ol.bib_record_id=l.bib_record_id
-		LIMIT 1
-	) as ordernum,
-	p.call_number_norm,
-	i.copy_num,
-	(
-		SELECT vv.field_content
-		FROM sierra_view.varfield as vv
-		WHERE vv.record_id = l.item_record_id AND vv.varfield_type_code = 'v'
-		LIMIT 1
-	) as volume,
-	p.barcode,
-	i.icode2,
-	i2.item_status_code,
-	rb.record_type_code || rb.record_num || 'a' as bib_record_num,
-	ri.record_type_code || ri.record_num || 'a' as item_record_num,
-	(
-		SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1}Hathi Trust Report)', '', 'ig')
-		FROM sierra_view.varfield as v
-		WHERE v.record_id = l.bib_record_id
-		AND v.marc_tag='910' AND v.field_content like '%Hathi%'
-		ORDER BY v.occ_num
-		LIMIT 1
-	) as localtag,
-	(
-		SELECT (sv.content)
-		FROM sierra_view.subfield_view as sv
-		WHERE sv.record_id = l.bib_record_id AND sv.marc_tag='245' AND sv.tag='a'
-		ORDER BY sv.occ_num
-		LIMIT 1
-	) as title,
-	(
-		SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
-		FROM sierra_view.varfield as v
-		WHERE v.record_id = l.bib_record_id AND v.marc_tag='250'
-		ORDER BY v.occ_num
-		LIMIT 1
-	) as edition,
-	(
-		SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
-		FROM sierra_view.varfield as v
-		WHERE v.record_id = l.bib_record_id AND v.marc_tag='260'
-		ORDER BY v.occ_num
-		LIMIT 1
-	) as publisher,
-	(
-		SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
-		FROM sierra_view.varfield as v
-		WHERE v.record_id = l.bib_record_id AND v.marc_tag='300'
-		ORDER BY v.occ_num
-		LIMIT 1
-	) as description,
-	i.location_code iloc,
-	(
-		SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
-		FROM sierra_view.varfield as v
-		WHERE v.record_id = l.bib_record_id AND v.marc_tag='590'
-		ORDER BY v.occ_num
-		LIMIT 1
-	) as localnotes
+		bo.display_order,
+		(
+			SELECT boi.name
+			FROM sierra_view.bool_info as boi
+			WHERE boi.id = bo.bool_info_id
+		) as project_title,
+		(
+			SELECT boi.bool_gmt
+			FROM sierra_view.bool_info as boi
+			WHERE boi.id = bo.bool_info_id
+		) as list_date,
+		(
+			SELECT o.record_type_code || o.record_num || 'a'
+			FROM sierra_view.order_view as o, sierra_view.bib_record_order_record_link as ol
+			WHERE o.id = ol.order_record_id AND ol.bib_record_id=l.bib_record_id
+			LIMIT 1
+		) as ordernum,
+		p.call_number_norm,
+		i.copy_num,
+		(
+			SELECT vv.field_content
+	 		FROM sierra_view.varfield as vv
+	 		WHERE vv.record_id = l.item_record_id AND vv.varfield_type_code = 'v'
+			LIMIT 1
+		) as volume,
+		p.barcode,
+		i.icode2,
+		i2.item_status_code,
+		rb.record_type_code || rb.record_num || 'a' as bib_record_num,
+		ri.record_type_code || ri.record_num || 'a' as item_record_num,
+		(
+			SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1}Hathi Trust Report)', '', 'ig')
+			FROM sierra_view.varfield as v
+			WHERE v.record_id = l.bib_record_id
+				AND v.marc_tag='910'
+				AND v.field_content like '%Hathi%'
+			ORDER BY v.occ_num
+			LIMIT 1
+		) as localtag,
+		(
+			SELECT sv.content
+			FROM sierra_view.subfield_view as sv
+			WHERE sv.record_id = l.bib_record_id
+				AND sv.marc_tag='245'
+				AND sv.tag='a'
+			ORDER BY sv.occ_num
+			LIMIT 1
+		) as title,
+		(
+			SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
+			FROM sierra_view.varfield as v
+			WHERE v.record_id = l.bib_record_id AND v.marc_tag='250'
+			ORDER BY v.occ_num
+			LIMIT 1
+		) as edition,
+		(
+			SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
+			FROM sierra_view.varfield as v
+			WHERE v.record_id = l.bib_record_id AND v.marc_tag='260'
+			ORDER BY v.occ_num
+			LIMIT 1
+		) as publisher,
+		(
+			SELECT cf.p07 || cf.p08 || cf.p09 || cf.p10 as pubYear
+			FROM sierra_view.control_field as cf
+			WHERE cf.record_id = l.bib_record_id AND cf.control_num = '8'
+			ORDER BY cf.occ_num
+			LIMIT 1
+		) as pubyear,
+		(
+			SELECT brp.best_author
+			FROM sierra_view.bib_record_property as brp
+			WHERE brp.bib_record_id = l.bib_record_id
+		) as author,
+		(
+			SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
+			FROM sierra_view.varfield as v
+			WHERE v.record_id = l.bib_record_id AND v.marc_tag='300'
+			ORDER BY v.occ_num
+			LIMIT 1
+		) as description,
+		i.location_code iloc,
+		(
+			SELECT regexp_replace(trim(v.field_content), '(\|[a-z]{1})', '', 'ig')
+			FROM sierra_view.varfield as v
+			WHERE v.record_id = l.bib_record_id AND v.marc_tag='590'
+			ORDER BY v.occ_num
+			LIMIT 1
+		) as localnotes
 	FROM sierra_view.item_record as i
-	LEFT JOIN sierra_view.bib_record_item_record_link as l ON (l.item_record_id = i.record_id)
-	LEFT JOIN sierra_view.item_view as i2 ON (i2.id=i.record_id)
-	LEFT JOIN sierra_view.item_record_property as p ON (p.item_record_id=i.record_id)
-	LEFT JOIN sierra_view.bool_set as bo ON (bo.record_metadata_id=i.record_id)
-	LEFT JOIN sierra_view.record_metadata as ri ON (ri.id = i.record_id)
-	LEFT JOIN sierra_view.record_metadata as rb ON (rb.id = l.bib_record_id) AND (rb.campus_code = '')
+		JOIN sierra_view.bib_record_item_record_link as l ON (l.item_record_id = i.record_id)
+		JOIN sierra_view.item_view as i2 ON (i2.id=i.record_id)
+		JOIN sierra_view.item_record_property as p ON (p.item_record_id=i.record_id)
+		JOIN sierra_view.bool_set as bo ON (bo.record_metadata_id=i.record_id)
+		JOIN sierra_view.record_metadata as ri ON (ri.id = i.record_id)
+		JOIN sierra_view.record_metadata as rb ON (rb.id = l.bib_record_id) AND (rb.campus_code = '')
 	WHERE bo.bool_info_id={listID};
 
 	CREATE TEMP TABLE temp_dupe AS
 	SELECT count(l.bib_record_id)>1 as BNDWITH, bo.display_order
 	FROM sierra_view.bib_record_item_record_link as l
-	JOIN sierra_view.bool_set as bo ON (bo.record_metadata_id=l.item_record_id)
+		JOIN sierra_view.bool_set as bo ON (bo.record_metadata_id=l.item_record_id)
 	WHERE bo.bool_info_id={listID}
 	GROUP BY l.item_record_id, bo.display_order;
 
-	SELECT t.*, du.BNDWITH
+	SELECT Current_Date as Flag_date, t.*, du.BNDWITH
 	FROM temp_item_data as t
-	JOIN temp_dupe as du ON (du.display_order = t.display_order)
+		JOIN temp_dupe as du ON (du.display_order = t.display_order)
 	ORDER BY t.display_order;`
 
 	sqlSelect = strings.ReplaceAll(sqlSelect, "{listID}", strconv.Itoa(listID))
@@ -166,22 +196,27 @@ func PullSlipsForList(connString string, listID int) ([]PullSlipRow, error) {
 
 func scanPullSlipRow(rows *sql.Rows) (PullSlipRow, error) {
 	var displayOrder, copyNum int
-	var bndWidth bool
-	var orderNum, callNumber, volume, barCode, code2, itemStatusCode,
-		bibRecordNum, itemRecordNum, localTag, title, edition,
-		publisher, description, itemLocation, localNotes sql.NullString
+	var bndWith bool
+	var flagDate, listDate sql.NullTime
+	var projectTitle, orderNum, callNumber, volume, barCode, code2,
+		itemStatusCode, bibRecordNum, itemRecordNum, localTag, title, edition,
+		publisher, pubYear, author, description, itemLocation, localNotes sql.NullString
 
-	err := rows.Scan(&displayOrder, &orderNum, &callNumber, &copyNum, &volume,
+	err := rows.Scan(&flagDate, &displayOrder, &projectTitle, &listDate,
+		&orderNum, &callNumber, &copyNum, &volume,
 		&barCode, &code2, &itemStatusCode, &bibRecordNum, &itemRecordNum,
-		&localTag, &title, &edition, &publisher, &description, &itemLocation,
-		&localNotes, &bndWidth)
+		&localTag, &title, &edition, &publisher, &pubYear, &author,
+		&description, &itemLocation, &localNotes, &bndWith)
 
 	if err != nil {
 		return PullSlipRow{}, err
 	}
 
 	row := PullSlipRow{}
+	row.FlagDate = flagDate
 	row.DisplayOrder = displayOrder
+	row.ProjectTitle = stringValue(projectTitle)
+	row.ListDate = listDate
 	row.OrderNum = stringValue(orderNum)
 	row.CallNumber = stringValue(callNumber)
 	row.CopyNum = copyNum
@@ -195,10 +230,12 @@ func scanPullSlipRow(rows *sql.Rows) (PullSlipRow, error) {
 	row.Title = stringValue(title)
 	row.Edition = stringValue(edition)
 	row.Publisher = stringValue(publisher)
+	row.PubYear = stringValue(pubYear)
+	row.Author = stringValue(author)
 	row.Description = stringValue(description)
 	row.ItemLocation = stringValue(itemLocation)
 	row.LocalNotes = stringValue(localNotes)
-	row.BndWidth = bndWidth
+	row.BndWith = bndWith
 
 	return row, nil
 }
